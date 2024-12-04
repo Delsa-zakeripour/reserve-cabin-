78075ebca20dc1daa3abc4bf59f8c83c4c2bab88
Delsa <delsa.zakeripour@gmail.com>
2024-12-04 18:18:07 +0330

    update password and avatar

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
@@ -30,4 +30,37 @@ export async function logout() {
   const { error } = await supabase.auth.signOut();
 
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, email, avatar }) {
+  //  1 update username or password
+  let updateDeta;
+
+  if (password) updateDeta = { password };
+  if (email) updateDeta = { data: { email } };
+
+  const { data, error } = await supabase.auth.updateUser(updateDeta);
+
+  if (error) throw new Error(error.message);
+  if (avatar) return data;
+
+  // 2 upload an avatar
+  const fileName = `avatar-${data.user.id}-${Math.random}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // 3 update avatar in the user
+  const { data: updateUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatar/${fileName}`,
+    },
+  });
+
+  if (error2) throw new Error(error2.message);
+
+  return updateUser;
 }
